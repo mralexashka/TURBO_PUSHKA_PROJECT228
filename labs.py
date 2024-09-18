5973e67b6c8c31da9d9508defff281d649312037
mralexashka <xandr.cosmonaut@gmail.com>
2024-09-18 20:48:45 +0300

    i wanna be the 1

--- labs.py
+++ labs.py
@@ -1,6 +1,45 @@
-n = int(input())
-matrix = []
-for i in range(n):
-    temp = [int(num) for num in input().split()]
-    matrix.append(temp)
+import math
 
+
+def print_matrix(matrix, width):
+    for r in range(len(matrix)):
+        for c in range(len(matrix[0])):
+            print(str(matrix[r][c]).ljust(width), end='')
+        print()
+
+
+a = list()
+n = int(input())
+flag = True
+a_p = list()
+a_pp = list()
+for i in range(n):
+    a.append([int(n) for n in input().split()])
+    a_p.append((i + 1) ** 2)
+a_s = list()
+sum_row = 0
+sum_diag = 0
+sum_diag2 = 0
+for i in range(n):
+    sum_col = 0
+    sum_diag += a[i][i]
+    sum_diag2 += a[n-1-i][i]
+    a_s.append(sum(a[i]))
+    for j in range(n):
+        sum_col += a[j][i]
+        if a[i][j] in a_p:
+            a_pp.append(a[i][j])
+    a_s.append(sum_col)
+a_s.append(sum_diag2)
+a_s.append(sum_diag)
+
+for s in a_s:
+    if s != sum_diag:
+        flag = False
+        break
+for p in a_p:
+    if p not in a_pp:
+        flag = False
+        break
+print(flag)
+print(a_s)
--- postgre.py
+++ postgre.py
@@ -1,11 +1,11 @@
 import psycopg2
 
-conn = psycopg2.connect(dbname="postgres", user="postgres", password="vae23_5h", host="127.0.0.1", port="5432")
+conn = psycopg2.connect(dbname="turbo_puska_db", user="postgres", password="alexashka", host="127.0.0.1", port="5432")
 cursor = conn.cursor()
 conn.autocommit = True
 
-# команда для создания базы данных metanit
-sql = "CREATE DATABASE metanit"
+
+sql = "CREATE TABLE users (id SERIAL PRIMARY KEY, login VARCHAR(50),  password VARCHAR(30))"
 
 # выполняем код sql
 cursor.execute(sql)
@@ -13,6 +13,5 @@ print("База данных успешно создана")
 
 cursor.close()  # закрываем курсор
 conn.close()  # закрываем подключение
-conn.close()
 
 
